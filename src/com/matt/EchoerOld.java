47e5e7199bce457b41b40c5b2da4076f2fe7b956
Matt Hill <matthill94.dev@hotmail.com>
2022-06-13 17:06:05 +0100

    Update EchoerOld.java

--- src/com/matt/EchoerOld.java
+++ src/com/matt/EchoerOld.java
@@ -13,9 +13,12 @@ public class EchoerOld extends Thread {
     public EchoerOld(Socket socket) {
         this.socket = socket;
     }
+    static int instanceCounter = 0;
 
     @Override
     public void run() {
+
+        int counter = 0;
         try {
             BufferedReader input = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()));
@@ -25,9 +28,17 @@ public class EchoerOld extends Thread {
             while (true) {
                 echoString = Integer.parseInt(input.readLine());
                 System.out.println("Received client input: " + echoString);
+                DeleteData app = new DeleteData();
+
+                // have a counter to gamify this app least count wins if any errors are at the end you lose
+
+
+                counter = instanceCounter;
 
                 if(echoString == 1) {
+                    instanceCounter++;
                     System.out.println("You've started the sim and have been issued your initial kit");
+
                     Database db = new Database();
 
                     Database.updateAmmo();
@@ -36,31 +47,73 @@ public class EchoerOld extends Thread {
 
                     Database.showStatus();
 
+                    System.out.println("Your Objective is to travel 1Km in the least moves possible.. Good Luck!");
+
 
                 }
-
                 if(echoString == 2) {
+                    instanceCounter++;
+
                     Database.showAmmo();
                 }
                 if(echoString == 3) {
+                    instanceCounter++;
+
                     Database.showRations();
                 }
                 if(echoString == 4) {
+                    instanceCounter++;
+
                     Database.showWater();
                 }
                 if(echoString == 5) {
-                    Database.showLocation();
+                    instanceCounter++;
 
+                    Database.showLocation();
                 }
                 if(echoString == 6) {
+                    instanceCounter++;
+
                     Database.updateLocation();
                     DatabaseExerciseArea.updateExerciseLocation();
+                    Database.removeRations();
+                    Database.removeWater();
                     Database.showLocation();
+                    System.out.println(" Game counter " + instanceCounter);
+                        // rations at 0 need rations to move
+
                 }
-                if (echoString == 0) {
-                    break;
+                if(echoString == 7) {
+                    instanceCounter++;
+                    Database.updateRations();
+                }
+                if(echoString == 8) {
+                    instanceCounter++;
+                    Database.updateWater();
                 }
 
+                if(instanceCounter >= 20 ) {
+                    System.out.println("You lost try again");
+                    app.delete(1);
+                    app.deleteEA(1);
+
+
+                    System.exit(0);
+                }
+
+                if (echoString == 0) {
+                    app.delete(1);
+//                    app.delete(2);
+//                    app.delete(3);
+//                    app.delete(4);
+                    app.deleteEA(1);
+//                    app.deleteEA(2);
+//                    app.deleteEA(3);
+//                    app.deleteEA(4);
+                    System.exit(0);
+
+                    break;
+                }
                 try {
 
                 }
